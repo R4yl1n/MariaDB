bbaea4a2b2a3a13e9b6897042188c318f99cbeae
r4yl1n <92576889+R4yl1n@users.noreply.github.com>
2022-09-25 20:10:45 +0200

    fixed the delete request

--- Flask/FlaskD/DeleteRequests/deleterequests.py
+++ Flask/FlaskD/DeleteRequests/deleterequests.py
@@ -2,12 +2,18 @@ import requests
 import json
 
 def deleterequests(vorname,nachname,telnummer):
-    data = {
-        'vorname':vorname,
-        'nachname':nachname,
-        'telnummer':telnummer
+    url = "http://20.91.193.124:4000/api/datas"
+    payload = {
+        # 'vorname':vorname,
+        # 'nachname':nachname,
+        # 'telnummer':telnummer
+
+        'vorname':"raylin",
+        'nachname':"gloor",
+        'telnummer':"0763970026"
     }
-    r = requests.delete("http://127.0.0.1:4000/api/datas",data)
+    headers = {'content-type': 'application/json'}
+    r = requests.delete(url=url,data=json.dumps(payload),headers=headers)
 
 
 #wrong delete request 
