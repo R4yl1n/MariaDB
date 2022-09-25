8bb0de5030ac22cec394284129a1e1c808ff89de
r4yl1n <92576889+R4yl1n@users.noreply.github.com>
2022-09-25 21:26:14 +0200

    bug fix

--- Flask/FlaskD/DeleteRequests/deleterequests.py
+++ Flask/FlaskD/DeleteRequests/deleterequests.py
@@ -4,13 +4,10 @@ import json
 def deleterequests(vorname,nachname,telnummer):
     url = "http://20.91.193.124:4000/api/datas"
     payload = {
-        # 'vorname':vorname,
-        # 'nachname':nachname,
-        # 'telnummer':telnummer
+        'vorname':vorname,
+        'nachname':nachname,
+        'telnummer':telnummer
 
-        'vorname':"raylin",
-        'nachname':"gloor",
-        'telnummer':"0763970026"
     }
     headers = {'content-type': 'application/json'}
     r = requests.delete(url=url,data=json.dumps(payload),headers=headers)
